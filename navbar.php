b0fb1380f2393220e8e9443ac810203c647d4c15
707624376@qq.com <707624376@qq.com>
2019-05-16 15:37:46 +0800

    个人修复navbar

---
+++ navbar.php
@@ -0,0 +1,11 @@
+		<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+ 		<nav id="nav" class="sb" role="navigation" itemscope="" itemtype="">
+			<ul id="site-menu" class="nav-menu">
+				<li><a <?php if($this->is('index')): ?> class="current"<?php endif; ?> href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a></li>
+
+      <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+        <?php while($pages->next()): ?>
+        <li><span<?php if($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?>><a href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a></span></li>
+        <?php endwhile; ?>
+			</ul>
+		 </nav>
